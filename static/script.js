b001a7b647c46db155dc2b178547933a1a2c3782
Daniel Rios <danielrios.oficial@gmail.com>
2026-07-28 00:33:33 -0300

    Change Update Interval

--- static/script.js
+++ static/script.js
@@ -1,8 +1,16 @@
 document.addEventListener('alpine:init', () => {
-    const key = 'theme'
+    const themeKey = 'theme'
+    const refreshesKey = 'refreshes'
 
-    Alpine.store(key, {
-        current: localStorage.getItem(key) || 'dark',
+    const refreshes = {
+        cpu: 'every 1s',
+        memory: 'every 2s',
+        disk: 'load',
+        gpu: 'every 1s'
+    }
+
+    Alpine.store(themeKey, {
+        current: localStorage.getItem(themeKey) || 'dark',
 
         toggle() {
             let newTheme = 'light'
@@ -12,7 +20,50 @@ document.addEventListener('alpine:init', () => {
             }
 
             this.current = newTheme
-            localStorage.setItem(key, newTheme)
+            localStorage.setItem(themeKey, newTheme)
+        }
+    })
+
+    Alpine.store(refreshesKey, {
+        values: (() => {
+            try {
+                const storedValues = localStorage.getItem(refreshesKey)
+
+                if (storedValues) {
+                    return JSON.parse(storedValues)
+                }
+
+                return refreshes
+            }
+            catch(err) {
+                return refreshes
+            }
+        })(),
+
+        get(refresh) {
+            if (this.values[refresh] === 'load') {
+                return 0
+            }
+
+            return this.values[refresh].replace('every', '').replace('s', '').trim()
+        },
+        set(refresh, value) {
+            let newValue = `every ${value}s`
+
+            if (value == '0') {
+                newValue = 'load'
+            }
+
+            const newValues = {
+                ...this.values,
+                [refresh]: newValue
+            }
+
+            this.values = newValues
+            localStorage.setItem(refreshesKey, JSON.stringify(newValues))
+
+            // TODO: Find a way to HTMX updates without reload
+            location.reload()
         }
     })
 })
